0e13f4cbabdb40477d85abaf1c9a4056def02faf
Doga Ister <dogaister@inside-65-73-107.wireless.utoronto.ca>
2018-09-27 16:00:41 -0400

    changed

--- inst/scripts/network.R
+++ inst/scripts/network.R
@@ -25,25 +25,16 @@ setwd("/Users/dogaister/Desktop/Courses/Fall_2018/BCB410/Rview/inst/scripts")
 # Load all required packages.
 
 install.packages("igraph")
-#install.packages("network")
 install.packages("networkD3")
-#install.packages("sna")
-#install.packages("visNetwork")
-#install.packages("threejs")
-#install.packages("ndtv")
-
-#if (! require(networkD3, quietly=TRUE)) {
-#  install.packages("networkD3")
-#  library(networkD3)
-#}
 
 # ====  FUNCTIONS  =============================================================
 #Load the data
 links <- read.csv("/Users/dogaister/Desktop/Courses/Fall_2018/BCB410/Rview/inst/data/data_edges.csv", header=T, as.is=T)
 nodes <- read.csv("/Users/dogaister/Desktop/Courses/Fall_2018/BCB410/Rview/inst/data/data_nodes.csv", header=T, as.is=T)
-#see the first few to make sure
+#see the first few rows to make sure
 head(nodes)
 head(links)
+########### PLOT ###########
 #Create igraph object
 library("igraph")
 #Using igraph's properties to define lines and nodes
@@ -60,6 +51,7 @@ V(net)$color <- mycolours[V(net)$node.type]
 #draw a simple non-interactive plot to check the value
 plot(net, edge.arrow.size=.4, edge.curved=.4, vertex.label=NA, vertex.frame.color="#ffffff") #don't forget to get the labels back
 
+########### Interactive ###########
 #load R API of D3
 library("networkD3")
 #D3 needs numeric values in order to work and it starts from 0 not 1 so subtract 1
@@ -70,10 +62,29 @@ links.d3 <- data.frame(from=as.numeric(factor(links$from))-1,
 ColourScale <- 'd3.scaleOrdinal().range(["steelblue","#FF6900"]);'
 #Make the order same as source file
 nodes.d3 <- cbind(idn=factor(nodes$name, levels=nodes$name), nodes)
+#Click action can color the selected node to another color
+myClick <- ' d3.select(this).select("circle").transition()
+.style("fill", "red");'
+
 #D3 property
-forceNetwork(Links = links.d3, Nodes = nodes.d3, Source="from", Target="to",
-             NodeID = "name", Group = "node.type", linkWidth = 1, linkColour = "#afafaf",
-             fontSize=12, zoom=T, colourScale = JS(ColourScale), legend=T,
-             opacity = 0.8, charge=-300, arrows = FALSE, bounded = FALSE, opacityNoHover = 2,
-             width = NULL, height = NULL)
+forceNetwork(Links = links.d3,
+             Nodes = nodes.d3,
+             Source="from",
+             Target="to",
+             NodeID = "name",
+             Group = "node.type",
+             linkWidth = 1,
+             linkColour = "#afafaf",
+             fontSize=12,
+             zoom=T,
+             colourScale = JS(ColourScale),
+             legend=T,
+             opacity = 0.8,
+             charge=-300,
+             arrows = FALSE,
+             bounded = TRUE,
+             opacityNoHover = 2,
+             width = NULL,
+             height = NULL,
+             clickAction = myClick)
 
