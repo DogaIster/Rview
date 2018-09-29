7d7f06223579290242c07b1d7872913bf770f2c4
Doga Ister <dogaister@Dogas-MacBook-Pro.local>
2018-09-29 18:53:19 -0400

    Corrected the test data, both plot and interactive network works, user prompt ready

--- inst/scripts/network.R
+++ inst/scripts/network.R
@@ -32,34 +32,38 @@ install.packages("networkD3")
 links <- read.csv("/Users/dogaister/Desktop/Courses/Fall_2018/BCB410/Rview/inst/data/data_edges.csv", header=T, as.is=T)
 nodes <- read.csv("/Users/dogaister/Desktop/Courses/Fall_2018/BCB410/Rview/inst/data/data_nodes.csv", header=T, as.is=T)
 #see the first few rows to make sure
-head(nodes)
-head(links)
-########### PLOT ###########
-#Create igraph object
-library("igraph")
-#Using igraph's properties to define lines and nodes
-net <- graph_from_data_frame(d=links, vertices=nodes, directed=T)
-#Get rid off selp loops and duplicates
-net <- simplify(net, remove.multiple = F, remove.loops = T)
-#See what it became to
-net
+#head(nodes)
+#head(links)
 
-#make the reactions orange, proteins steelblue
-mycolours <- c("steelblue", "orange")
-#color them according to their type
-V(net)$color <- mycolours[V(net)$node.type]
-#draw a simple non-interactive plot to check the value
-plot(net, edge.arrow.size=.4, edge.curved=.4, vertex.label=NA, vertex.frame.color="#ffffff") #don't forget to get the labels back
+question <- prompt("Do you want to draw a plot or an interactive metabolic network?")
+if( question == "Plot"){
+  ########### PLOT ###########
+  #Create igraph object
+  library("igraph")
+  #Using igraph's properties to define lines and nodes
+  net <- graph_from_data_frame(d=links, vertices=nodes, directed=T)
+  #Get rid off selp loops and duplicates
+  net <- simplify(net, remove.multiple = F, remove.loops = T)
+  #See what it became to
+  net
+
+  #make the reactions orange, proteins steelblue
+  mycolours <- c("steelblue", "orange")
+  #color them according to their type
+  V(net)$color <- mycolours[V(net)$node.group]
+  #draw a simple non-interactive plot to check the value
+  plot(net, edge.arrow.size=.4, edge.curved=.4, vertex.label=NA, vertex.frame.color="#ffffff") #don't forget to get the labels back
+} else{
 
 ########### Interactive ###########
 #load R API of D3
 library("networkD3")
 #D3 needs numeric values in order to work and it starts from 0 not 1 so subtract 1
-links.d3 <- data.frame(from=as.numeric(factor(links$from))-1,
-                       to=as.numeric(factor(links$to))-1 )
+links.d3 <- data.frame(from = match(links$from, nodes$id) - 1, to = match(links$to, nodes$id) - 1, value = 1)
+
 
 #node colors
-ColourScale <- 'd3.scaleOrdinal().range(["steelblue","#FF6900"]);'
+ColourScale <- 'd3.scaleOrdinal().range(["steelblue","orange"]);'
 #Make the order same as source file
 nodes.d3 <- cbind(idn=factor(nodes$name, levels=nodes$name), nodes)
 #Click action can color the selected node to another color
@@ -71,9 +75,9 @@ forceNetwork(Links = links.d3,
              Nodes = nodes.d3,
              Source="from",
              Target="to",
+             Value = "value",
              NodeID = "name",
              Group = "node.type",
-             linkWidth = 1,
              linkColour = "#afafaf",
              fontSize=12,
              zoom=T,
@@ -81,10 +85,11 @@ forceNetwork(Links = links.d3,
              legend=T,
              opacity = 0.8,
              charge=-300,
-             arrows = FALSE,
+             arrows = TRUE,
              bounded = TRUE,
              opacityNoHover = 2,
              width = NULL,
              height = NULL,
              clickAction = myClick)
+}
 
